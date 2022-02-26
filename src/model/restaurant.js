666c441449f0ad2e6b932b5db2e460bdcf69551f
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2022-02-26 14:24:17 +0400

    Add Restaurant and Review models

---
+++ src/model/restaurant.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const RestaurantSchema = new mongoose.Schema(
+  {
+    creatorUid: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+  { timestamps: true }
+);
+
+const Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+
+module.exports = Restaurant;
---
+++ src/model/review.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    creatorUid: {
+      type: String,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    dateOfCreation: {
+      type: Number,
+      required: true,
+    },
+    dateTimestamp: {
+      type: Number,
+      required: true,
+    },
+    score: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model("Review", ReviewSchema);
+
+module.exports = Review;
